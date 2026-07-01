7366937cd8043128d2df3352c03e5c82be322311
pauljevicj <pauljevicj12@gmail.com>
2026-07-01 19:12:40 +0200

    Initialize Spring Boot project

---
+++ src/main/java/rs/ac/bg/fon/camerarentbackend/CameraRentBackendApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.camerarentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CameraRentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CameraRentBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rs/ac/bg/fon/camerarentbackend/CameraRentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.camerarentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CameraRentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
