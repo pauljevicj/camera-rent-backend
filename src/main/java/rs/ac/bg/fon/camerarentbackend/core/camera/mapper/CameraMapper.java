0a9e7996700b298ad4341951f893a39905864e72
pauljevicj <pauljevicj12@gmail.com>
2026-07-02 22:18:33 +0200

    Fix Camera Service and Mapper

--- src/main/java/rs/ac/bg/fon/camerarentbackend/core/camera/mapper/CameraMapper.java
+++ src/main/java/rs/ac/bg/fon/camerarentbackend/core/camera/mapper/CameraMapper.java
@@ -1,6 +1,7 @@
 package rs.ac.bg.fon.camerarentbackend.core.camera.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import rs.ac.bg.fon.camerarentbackend.core.camera.dto.CameraRequestDto;
 import rs.ac.bg.fon.camerarentbackend.core.camera.dto.CameraResponseDto;
 import rs.ac.bg.fon.camerarentbackend.core.camera.entity.Camera;
@@ -11,4 +12,6 @@ public interface CameraMapper {
     CameraResponseDto toResponseDto(Camera camera);
 
     Camera toEntity(CameraRequestDto requestDto);
+
+    void update(@MappingTarget Camera camera, CameraRequestDto requestDto);
 }
---
+++ src/main/java/rs/ac/bg/fon/camerarentbackend/core/camera/service/CameraServiceImpl.java
@@ -0,0 +1,58 @@
+package rs.ac.bg.fon.camerarentbackend.core.camera.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import rs.ac.bg.fon.camerarentbackend.core.camera.cameramodel.repository.CameraModelRepository;
+import rs.ac.bg.fon.camerarentbackend.core.camera.dto.CameraRequestDto;
+import rs.ac.bg.fon.camerarentbackend.core.camera.dto.CameraResponseDto;
+import rs.ac.bg.fon.camerarentbackend.core.camera.entity.Camera;
+import rs.ac.bg.fon.camerarentbackend.core.camera.mapper.CameraMapper;
+import rs.ac.bg.fon.camerarentbackend.core.camera.repository.CameraRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CameraServiceImpl implements CameraService {
+
+    private final CameraRepository cameraRepository;
+    private final CameraModelRepository cameraModelRepository;
+    private final CameraMapper cameraMapper;
+
+    @Override
+    public CameraResponseDto create(CameraRequestDto requestDto) {
+        Camera savedCamera = cameraRepository.save(cameraMapper.toEntity(requestDto));
+        return cameraMapper.toResponseDto(savedCamera);
+    }
+
+    @Override
+    public CameraResponseDto update(Long id, CameraRequestDto requestDto) {
+        Camera camera = cameraRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Camera not found with id: " + id));
+
+        cameraMapper.update(camera, requestDto);
+        return cameraMapper.toResponseDto(cameraRepository.save(camera));
+    }
+
+    @Override
+    public CameraResponseDto getById(Long id) {
+        return cameraMapper.toResponseDto(cameraRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Camera not found with id: " + id)));
+    }
+
+    @Override
+    public List<CameraResponseDto> getAll() {
+        return cameraRepository.findAll()
+                .stream()
+                .map(cameraMapper::toResponseDto)
+                .toList();
+    }
+
+    @Override
+    public void delete(Long id) {
+        if (!cameraRepository.existsById(id)) {
+//            throw new RuntimeException(Camera.class.getSimpleName(), "id", id.toString());
+        }
+        cameraRepository.deleteById(id);
+    }
+}
