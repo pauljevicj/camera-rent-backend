fce9ed84d040a33c60f1885b4e570ecff58cbd76
pauljevicj <pauljevicj12@gmail.com>
2026-07-07 00:19:51 +0200

    Remove unused classes

--- src/main/java/rs/ac/bg/fon/camerarentbackend/core/client/clienttype/dto/ClientTypeRequestDto.java
+++
@@ -1,5 +0,0 @@
-package rs.ac.bg.fon.camerarentbackend.core.client.clienttype.dto;
-
-public record ClientTypeRequestDto(
-        String name
-) {}
--- src/main/java/rs/ac/bg/fon/camerarentbackend/core/client/clienttype/dto/ClientTypeResponseDto.java
+++
@@ -1,6 +0,0 @@
-package rs.ac.bg.fon.camerarentbackend.core.client.clienttype.dto;
-
-public record ClientTypeResponseDto(
-        Long id,
-        String name
-) {}
